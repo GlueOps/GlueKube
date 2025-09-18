59e16a02532580d55fb049d21d401d23c88bf6f9
Bazmahou <hamzabouissi@sousse.r-iset.tn>
2025-09-18 14:03:12 +0100

    feat: add ufw rules and change kubeadm to use private IPs and removed lb setup

--- terraform/local-file.tf
+++ terraform/local-file.tf
@@ -3,7 +3,6 @@ resource "local_file" "hosts_cfg" {
     {
       master_ipv4_addresses = {for key, server in hcloud_server.master-node : key => {private = tolist(server.network)[0].ip, public = server.ipv4_address}}
       worker_ipv4_addresses = {for key, server in hcloud_server.worker-node : key => {private = tolist(server.network)[0].ip, public = server.ipv4_address}}
-      loadbalancer_ipv4_address = {for key, server in hcloud_server.lb-node : key => {private = tolist(server.network)[0].ip, public = server.ipv4_address}}
     }
   )
   filename = "../ansible/inventory/hosts.yaml"
